f1c509c294b7fb0840010a3aaf584a3df7a6c476
flawada <flawa@fedora.fritz.box>
2026-06-20 22:48:25 +0200

    new install approach

--- setup.sh
+++ setup.sh
@@ -1,56 +1,76 @@
 #!/bin/bash
 
-clear
-
 set -e
-repo="config"
 
-if [ "$(uname)" != "Linux" ]; then
-  echo "this script must be run on linux"
-  exit 1
-fi
+# colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+repo='config'
 
 OS=$(grep -Po '(?<=^ID=).*' /etc/os-release | tr -d '"')
 
+case '$OS' in
+  fedora)
+    printf "%bFedora%b\n" "$GREEN" "$NC"
+    ;;
+  *)
+    printf "%bError%b\n" "$RED" "$NC"
+    exit 1
+    ;;
+esac
+
 cd /tmp
 
-sudo dnf in -y git
 
-if ! [ -d "/tmp/$repo" ]; then
-  git clone https://github.com/flawada/$repo
-fi
 
-if ! [ -d "/tmp/$repo/blueprints/$OS" ]; then
-  echo "$OS is not supported yet"
-  echo "suported distros: "
-  ls /tmp/$repo/blueprints
-  exit 1
-fi
 
+
+
+
+
+
+
+#### deprecated ####
+
+#sudo dnf in -y git
+#
+#if ! [ -d "/tmp/$repo" ]; then
+#  git clone https://github.com/flawada/$repo
+#fi
+#
+#if ! [ -d "/tmp/$repo/blueprints/$OS" ]; then
+#  echo "$OS is not supported yet"
+#  echo "suported distros: "
+#  ls /tmp/$repo/blueprints
+#  exit 1
+#fi
+#
 #####
-
-clear
-
-cd /tmp/$repo/blueprints/$OS
-
-blueprints=()
-i=0
-
-for blueprint in */; do
-  blueprints+=("$blueprint")
-
-  echo "$i) ${blueprint%/}"
-
-  i=$((i+1))
-done
-
-read -p "select blueprint: " item
-
-clear
-
-cd ${blueprints[$item]}
-
-sudo dnf update -y
-
-chmod +x setup.sh
-./setup.sh
+#
+#clear
+#
+#cd /tmp/$repo/blueprints/$OS
+#
+#blueprints=()
+#i=0
+#
+#for blueprint in */; do
+#  blueprints+=("$blueprint")
+#
+#  echo "$i) ${blueprint%/}"
+#
+#  i=$((i+1))
+#done
+#
+#read -p "select blueprint: " item
+#
+#clear
+#
+#cd ${blueprints[$item]}
+#
+#sudo dnf update -y
+#
+#chmod +x setup.sh
+#./setup.sh
