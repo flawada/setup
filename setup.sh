9a322fbffd18879e6b3c9c583b95e0d82c7eb197
flawa <e.sauermann@icloud.com>
2026-07-10 18:17:28 +0200

    Refactor setup script for better readability

--- setup.sh
+++ setup.sh
@@ -9,8 +9,6 @@ BLUE='\033[0;94m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 
-#####
-
 clear
 
 printf "$(cat << EOF
@@ -23,17 +21,15 @@ EOF
 )"
 
 printf "\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n\n"
-printf "%bChecking System..%b\n" "$BLUE" "$NC"
 
+printf "%bChecking System..%b\n" "$BLUE" "$NC"
 if [ -f /etc/os-release ]; then
     source /etc/os-release
 else
     printf "%bError: /etc/os-release does not exist. %b\n" "$RED" "$NC"
     exit 1
 fi
-
 OS=($(curl -s "https://api.github.com/repos/flawada/blueprint/contents/blueprints" | grep "name" | cut -d '"' -f 4))
-
 if [[ "${OS[*]}" == *"$ID"* ]]; then
     printf "%bSystem: %s | supported%b\n" "$GREEN" "$PRETTY_NAME" "$NC"
 else
@@ -41,16 +37,15 @@ else
     exit 1
 fi
 
-blueprints=($(curl -s "https://api.github.com/repos/flawada/blueprint/contents/blueprints/$ID" | grep "name" | cut -d '"' -f 4))
-
 printf "\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n\n"
 
+printf "%bLoading blueprints%b\n" "$GREEN" "$NC"
+blueprints=($(curl -s "https://api.github.com/repos/flawada/blueprint/contents/blueprints/$ID" | grep "name" | cut -d '"' -f 4))
 if [ "${#blueprints[@]}" -eq 0 ]; then
     printf "%bError: No blueprint found. %b\n" "$RED" "$NC"
     exit 1
 elif [ "${#blueprints[@]}" -eq 1 ]; then
     blueprint="${blueprints[0]}"
-    printf "%bFound %s blueprint..%b\n" "$BLUE" "$blueprint" "$NC"
 else
     printf "%bSelect a blueprint:%b\n" "$BLUE" "$NC"
     select blueprint in "${blueprints[@]}"; do
@@ -61,11 +56,11 @@ else
         fi
     done
 fi
+printf "%bBlueprint %s selected..%b\n" "$GREEN" "$blueprint" "$NC"
 
-printf "%bBlueprint selected%b\n" "$GREEN" "$NC"
 printf "\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n\n"
-printf "%bRedirecting..%b\n" "$BLUE" "$NC"
 
+printf "%bRedirecting..%b\n" "$BLUE" "$NC"
 if sudo -v; then
     bash <(curl -LfsS https://raw.githubusercontent.com/flawada/blueprint/main/blueprints/$ID/$blueprint/setup.sh)
 fi
