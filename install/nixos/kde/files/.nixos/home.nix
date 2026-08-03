edc0c4f0f3be85d3bb13a1973063372bcee008c4
flawa <e.sauermann@icloud.com>
2026-08-04 01:44:32 +0200

    removing file folder, leaving only the tarball

--- install/nixos/kde/files/.nixos/home.nix
+++
@@ -1,8 +0,0 @@
-{ config, pkgs, ... }:
-
-{
-  home.username = "USERNAME";
-  home.homeDirectory = "/home/USERNAME";
-  home.stateVersion = "26.11";
-  #home.file.".config/hypr".source = ./config/hypr;
-}
